0da1f221859dd90dc1ed1eca45b85e25190121d1
Lynn Root <lynn@spotify.com>
2014-08-04 13:54:25 -0700

    Updated version for 3.4 improvement

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def read(*filenames, **kwargs):
 long_description = read('README.md', 'CHANGES.md')
 
 setup(name='pyladies',
-      version='2.0.2',
+      version='2.0.3',
       author=u'Lynn Root',
       author_email='lynn@pyladies.com',
       license="Creative Commons Attribution-ShareAlike 3.0 Unported",
